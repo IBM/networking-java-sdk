69e75e6659c99add63a6ce44911c4a7850bbec40
Joseph Kalandarishvili <jkalanda@Josephs-MBP.hsd1.mn.comcast.net>
2023-05-15 13:29:27 -0500

    more tweaks
    Signed-off-by: Joseph Kalandarishvili <jkalanda@Josephs-MBP.hsd1.mn.comcast.net>

--- modules/transit-gateway-apis/src/test/java/com/ibm/cloud/networking/transit_gateway_apis/v1/TransitGatewayApisTest.java
+++ modules/transit-gateway-apis/src/test/java/com/ibm/cloud/networking/transit_gateway_apis/v1/TransitGatewayApisTest.java
@@ -87,21 +87,18 @@ import java.util.Map;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.testng.PowerMockTestCase;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
+
 
 /**
  * Unit test class for the TransitGatewayApis service.
  */
 @PrepareForTest({ EnvironmentUtils.class })
 @PowerMockIgnore({"javax.net.ssl.*", "org.mockito.*"})
-public class TransitGatewayApisTest extends PowerMockTestCase {
+public class TransitGatewayApisTest {
 
   final HashMap<String, InputStream> mockStreamMap = TestUtilities.createMockStreamMap();
   final List<FileWithMetadata> mockListFileWithMetadata = TestUtilities.creatMockListFileWithMetadata();
@@ -117,8 +114,7 @@ public class TransitGatewayApisTest extends PowerMockTestCase {
   }
 
   public void constructClientService() throws Throwable {
-    PowerMockito.spy(EnvironmentUtils.class);
-    PowerMockito.when(EnvironmentUtils.getenv()).thenReturn(getTestProcessEnvironment());
+    System.setProperty("TESTSERVICE_AUTH_TYPE", "noAuth");
     final String serviceName = "testService";
     // set mock values for global params
     String version = "testString";
