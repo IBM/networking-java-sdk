53ee076dece934b6bd395343a9a5be6ebc6373ef
Joseph Kalandarishvili <jkalanda@Josephs-MBP.hsd1.mn.comcast.net>
2023-05-15 15:49:51 -0500

    more tweaks
    Signed-off-by: Joseph Kalandarishvili <jkalanda@Josephs-MBP.hsd1.mn.comcast.net>

--- modules/zones-settings/src/test/java/com/ibm/cloud/networking/zones_settings/v1/ZonesSettingsTest.java
+++ modules/zones-settings/src/test/java/com/ibm/cloud/networking/zones_settings/v1/ZonesSettingsTest.java
@@ -157,10 +157,6 @@ import java.util.Map;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.testng.PowerMockTestCase;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -169,9 +165,7 @@ import static org.testng.Assert.*;
 /**
  * Unit test class for the ZonesSettings service.
  */
-@PrepareForTest({ EnvironmentUtils.class })
-@PowerMockIgnore({"javax.net.ssl.*", "org.mockito.*"})
-public class ZonesSettingsTest extends PowerMockTestCase {
+public class ZonesSettingsTest {
 
   final HashMap<String, InputStream> mockStreamMap = TestUtilities.createMockStreamMap();
   final List<FileWithMetadata> mockListFileWithMetadata = TestUtilities.creatMockListFileWithMetadata();
@@ -179,16 +173,8 @@ public class ZonesSettingsTest extends PowerMockTestCase {
   protected MockWebServer server;
   protected ZonesSettings zonesSettingsService;
 
-  // Creates a mock set of environment variables that are returned by EnvironmentUtils.getenv().
-  private Map<String, String> getTestProcessEnvironment() {
-    Map<String, String> env = new HashMap<>();
-    env.put("TESTSERVICE_AUTH_TYPE", "noAuth");
-    return env;
-  }
-
   public void constructClientService() throws Throwable {
-    PowerMockito.spy(EnvironmentUtils.class);
-    PowerMockito.when(EnvironmentUtils.getenv()).thenReturn(getTestProcessEnvironment());
+    System.setProperty("TESTSERVICE_AUTH_TYPE", "noAuth");
     final String serviceName = "testService";
     // set mock values for global params
     String crn = "testString";
--- modules/zones/src/test/java/com/ibm/cloud/networking/zones/v1/ZonesTest.java
+++ modules/zones/src/test/java/com/ibm/cloud/networking/zones/v1/ZonesTest.java
@@ -43,10 +43,6 @@ import java.util.Map;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.testng.PowerMockTestCase;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -55,9 +51,7 @@ import static org.testng.Assert.*;
 /**
  * Unit test class for the Zones service.
  */
-@PrepareForTest({ EnvironmentUtils.class })
-@PowerMockIgnore({"javax.net.ssl.*", "org.mockito.*"})
-public class ZonesTest extends PowerMockTestCase {
+public class ZonesTest
 
   final HashMap<String, InputStream> mockStreamMap = TestUtilities.createMockStreamMap();
   final List<FileWithMetadata> mockListFileWithMetadata = TestUtilities.creatMockListFileWithMetadata();
@@ -65,16 +59,8 @@ public class ZonesTest extends PowerMockTestCase {
   protected MockWebServer server;
   protected Zones zonesService;
 
-  // Creates a mock set of environment variables that are returned by EnvironmentUtils.getenv().
-  private Map<String, String> getTestProcessEnvironment() {
-    Map<String, String> env = new HashMap<>();
-    env.put("TESTSERVICE_AUTH_TYPE", "noAuth");
-    return env;
-  }
-
   public void constructClientService() throws Throwable {
-    PowerMockito.spy(EnvironmentUtils.class);
-    PowerMockito.when(EnvironmentUtils.getenv()).thenReturn(getTestProcessEnvironment());
+    System.setProperty("TESTSERVICE_AUTH_TYPE", "noAuth");
     final String serviceName = "testService";
     // set mock values for global params
     String crn = "testString";
